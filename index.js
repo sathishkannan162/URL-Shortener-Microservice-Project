97f7547f98bf9702318ed5e0a0a561036b5f1c0e
sathishkannan162 <sathishkannan162@gmail.com>
2022-10-22 19:56:53 +0530

    feat: (feat/refactor) url are saved to database and shorturls are returned. refactored single to double quotes

--- index.js
+++ index.js
@@ -1,65 +1,92 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const app = express();
-require('./database');
-let UrlModel = require('./url');
+require("./database");
+let UrlModel = require("./url");
 
-const dns = require('node:dns');
+const dns = require("node:dns");
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 //middleware on post
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.post('/api/shorturl',function(req,res){
+app.post("/api/shorturl", function (req, res) {
   let url = req.body.url;
-  let httpsRegex = /^https?:\/\//i
+  let httpsRegex = /^https?:\/\//i;
   if (httpsRegex.test(url)) {
-  url = url.replace(httpsRegex,'');
-  dns.lookup(url,function(err,address,family){
-    if (err) {
-      res.json({error: "Invalid URL"});
-    }
-    else {
-      res.json({
-        original_url: url,
-        short_url: Math.floor((Math.random()*20000) +1)
-      });
-    }
-  });
-}
-else {
-  res.json({error: "Invalid URL"});
-}
+    url = url.replace(httpsRegex, "");
+    dns.lookup(url, function (err, address, family) {
+      if (err) {
+        res.json({ error: "Invalid URL" });
+      } else {
+        UrlModel.findOne({
+          original_url: req.body.url,
+        })
+          .then((docs) => {
+            console.log("found", docs);
+            if (docs == null) {
+              let newURL = new UrlModel({
+                original_url: req.body.url,
+              });
+
+              newURL
+                .save()
+                .then((docs) => {
+                  console.log("saved", docs);
+                  res.json({
+                    original_url: docs.original_url,
+                    short_url: docs.short_url
+                  });
+                })
+                .catch((err) => {
+                  console.log(err);
+                  console.log("we have a error in save");
+                });
+              
+            }
+            else {
+              res.json({
+                original_url: docs.original_url,
+                short_url: docs.short_url
+              });
+            }
+          })
+          .catch((err) => {
+            console.log(err);
+            console.log("error in finding");
+          });
+      }
+    });
+  } 
 });
 
 //redirection
-app.get('/api/shorturl/:url',function(req,res){
-  if (req.params.url== 8) {
-  res.redirect('https://google.com');
-  }
-  else {
+app.get("/api/shorturl/:url", function (req, res) {
+  if (req.params.url == 8) {
+    res.redirect("https://google.com");
+  } else {
     res.json({
-      error: "No short URL found for the given input"
-    })
+      error: "No short URL found for the given input",
+    });
   }
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
